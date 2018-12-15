0b95cb9fdc12b8ad9fd0e4de178f83b713a7134b
Kounotori15 <waoon.kouno@nifty.com>
2018-12-15 17:14:24 +0900

    変更じゃんけんプログラムを消去しました

--- index.php
+++ index.php
@@ -8,18 +8,9 @@ $messageId = $jsonObj->{"events"}[0]->{"message"}->{"id"};
 $message_type = $jsonObj->{"events"}[0]->{"message"}->{"type"};
 $message_recv = $jsonObj->{"events"}[0]->{"message"}->{"text"};
 
-$flg = file_get_contents("data.txt");
 $message = "";
 if($message_type == "text"){
     $message = $message_recv;
-    if($message == "じゃんけん"){
-        $message = "じゃんけんするぞ";
-        file_put_contents("data.txt", "1");
-    }
-    else if($flg == "1"){
-        $message = janken($message);
-    }
-    
 }else{
     $message = "申し訳ありません。テキストのみをいれてください";
 }
@@ -46,48 +37,5 @@ function send_message($replyToken, $line_accessToken, $message){
     curl_setopt_array($curl, $options);
     curl_exec($curl);
     curl_close($curl);
-}
-?>
-
-<?php
-function janken($message){
-    $a = rand(0,2);
-    if($message == "グー"){
-        if($a == 0){
-            $message = "グー....あいこだね、もう一回やるぞ";
-        }
-        if($a == 1){
-            $message = "チョキ....負けちゃた、次は負けないからね";
-        }
-        if($a == 2){
-            $message = "パー....やったー勝ったー、もう1回勝負だ";
-        }
-    }
-    else if($message == "チョキ"){
-        if($a == 0){
-            $message = "グー....やったー勝ったー、もう1回勝負だ";
-        }
-        if($a == 1){
-            $message = "チョキ....あいこだね、もう一回やるぞ";
-        }
-        if($a == 2){
-            $message = "パー....負けちゃた、次は負けないからね";
-        }
-    }
-    else if($message == "パー"){
-        if($a == 0){
-            $message = "グー....負けちゃた、次は負けないからね";
-        }
-        if($a == 1){
-            $message = "チョキ....やったー勝ったー、もう1回勝負だ";
-        }
-        if($a == 2){
-            $message = "パー....あいこだね、もう一回やるぞ";
-        }
-    }
-    else{
-        file_put_contents("data.txt", "0");
-    }
-    return $message;
 }
 ?>
